001215d43fc255fa7f9771181326dbf7d7dbc785
ivanovsdesign <78684921+ivanovsdesign@users.noreply.github.com>
2024-06-12 23:54:17 +0300

    #1 Add routing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import DinoGame from './components/DinoGame.vue'
+import GameMenu from './components/GameMenu.vue'
 import WebApp from '@twa-dev/sdk'
+import router from './router'
 
-createApp(App).mount('#app')
+
+createApp(App).use(router).mount('#app')
 WebApp.ready()
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import IntroPage from '/src/components/IntroPage.vue';
+import GamePage from '/src/components/DinoGame.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Intro',
+    component: IntroPage,
+  },
+  {
+    path: '/game',
+    name: 'Game',
+    component: GamePage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
