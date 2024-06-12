a4e25170854a9ffb7a68fb7a441ea37e72337bb6
ivanovsdesign <78684921+ivanovsdesign@users.noreply.github.com>
2024-06-13 00:11:26 +0300

    #2 Fix routing paths

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import DinoGame from './components/DinoGame.vue'
-import GameMenu from './components/GameMenu.vue'
 import WebApp from '@twa-dev/sdk'
 import router from './router'
 
